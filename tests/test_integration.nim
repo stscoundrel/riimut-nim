e007fb490afcdb6218fded28ac5586f093edf454
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-26 18:04:34 +0200

    Integration tests: add elder futhark

--- tests/test_integration.nim
+++ tests/test_integration.nim
@@ -11,8 +11,22 @@ test "Parses to Younger Futhark":
 
 test "Parses Younger Futhark to text":
   # From Old Groms runestone.
-  const content = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ";
-  const expected = "auk tani karþi kristna";
+  const content = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ"
+  const expected = "auk tani karþi kristna"
   let result = riimut.youngerfuthark.runesToLetters(content)
+  check result == expected
+
+test "Parses to Elder Futhark":
+  # From 4th century axe in Jutland
+  const content = "wagagastiz alu wihgu sikijaz aiþalataz"
+  const expected = "ᚹᚨᚷᚨᚷᚨᛋᛏᛁᛉ:ᚨᛚᚢ:ᚹᛁᚻᚷᚢ:ᛋᛁᚲᛁᛃᚨᛉ:ᚨᛁᚦᚨᛚᚨᛏᚨᛉ"
+  let result = riimut.elderfuthark.lettersToRunes(content)
+  check result == expected
+
+test "Parses Elder Futhark to text":
+  # From 4th century axe in Jutland
+  const content = "ᚹᚨᚷᚨᚷᚨᛋᛏᛁᛉ:ᚨᛚᚢ:ᚹᛁᚻᚷᚢ:ᛋᛁᚲᛁᛃᚨᛉ:ᚨᛁᚦᚨᛚᚨᛏᚨᛉ"
+  const expected = "wagagastiz alu wihgu sikijaz aiþalataz"
+  let result = riimut.elderfuthark.runesToLetters(content)
   check result == expected
 
