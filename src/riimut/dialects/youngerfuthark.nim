0bc51731de16a7bd3ccdcded775bbd9a59796d02
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-21 17:19:15 +0300

    Add long branch / short twig YF variants

--- src/riimut/dialects/youngerfuthark.nim
+++ src/riimut/dialects/youngerfuthark.nim
@@ -1,16 +1,38 @@
 from ../transform import transform
-from youngerfuthark/lettermapping import getLettersToRunesMap
+from youngerfuthark/lettermapping import getLettersToLongBranchRunesMap, getLettersToShortTwigRunesMap
 from youngerfuthark/runemapping import getRunesToLettersMap
 
-proc lettersToRunes(content: string) : string =
-  const mapping = getLettersToRunesMap()
+type
+  RuneVariant = enum
+    longBranch, shortTwig
+
+proc lettersToLongBranchRunes(content: string) : string =
+  const mapping = getLettersToLongBranchRunesMap()
 
   result = transform(content, mapping)
+
+proc lettersToShortTwigRunes(content: string) : string =
+  const mapping = getLettersToShortTwigRunesMap()
+
+  result = transform(content, mapping)
+
+proc lettersToRunes(content: string) : string =
+  result = lettersToLongBranchRunes(content)
+
+proc lettersToRunes(content: string, variant: RuneVariant) : string =
+  if variant == RuneVariant.shortTwig:
+    result = lettersToShortTwigRunes(content)
+  else:
+    result = lettersToLongBranchRunes(content)
 
 proc runesToLetters(content: string) : string =
   const mapping = getRunesToLettersMap()
 
   result = transform(content, mapping)
 
+
+export RuneVariant
 export lettersToRunes
 export runesToLetters
+export lettersToLongBranchRunes
+export lettersToShortTwigRunes
